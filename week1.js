918ead85eca7b52f1ee003461a36dca74f4dd85b
dedrick irby <irb25001@byui.edu>
2026-09-19 15:00:55 -0600

    Complete JavaScript variables and constants practice

--- week1.js
+++ week1.js
@@ -1,24 +1,14 @@
-```javascript
-// WDD 131 - Variables and Constants
+// WDD 131 - JavaScript Variables and Constants
 
 // Constants and Variables
 const PI = 3.14;
 let radius = 3;
 
 console.log("PI:", PI);
-console.log("Radius:", radius);
+console.log("radius:", radius);
 
-let area = PI * radius * radius;
-console.log("Circle area:", area);
-
-// Change the radius
-radius = 5;
-
-console.log("New radius:", radius);
-
-area = PI * radius * radius;
-console.log("New circle area:", area);
-
+radius = 10;
+console.log("updated radius:", radius);
 
 // Type Coercion
 const one = 1;
@@ -26,11 +16,8 @@ const two = "2";
 
 console.log("one:", one);
 console.log("two:", two);
-
 console.log("one + two:", one + two);
-
 console.log("Number(one) + Number(two):", Number(one) + Number(two));
-
 
 // Global and Block Scope
 let course = "CSE131";
@@ -38,9 +25,8 @@ let course = "CSE131";
 if (true) {
     let student = "John";
 
-    console.log("Course inside block:", course);
-    console.log("Student inside block:", student);
+    console.log("course inside block:", course);
+    console.log("student inside block:", student);
 }
 
-console.log("Course outside block:", course);
-```
+console.log("course outside block:", course);
